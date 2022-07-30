6fdc88c20b2f475a56ebd24eecc39ff1bdfda649
jieHFUT <3492779706@qq.com>
2022-07-30 18:52:50 +0800

    官方顺序表

---
+++ officiallist/src/officiallist/OfficialList.java
@@ -0,0 +1,23 @@
+package officiallist;
+
+import java.util.ArrayList;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-07-30
+ * Time: 18:50
+ */
+
+
+public class OfficialList {
+
+    public static void main(String[] args) {
+
+        ArrayList arrayList = new ArrayList();
+
+
+    }
+
+}
