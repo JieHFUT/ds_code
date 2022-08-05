c46d34063bec973f70f80e06824204843846c75f
jieHFUT <3492779706@qq.com>
2022-08-05 19:14:42 +0800

    队列

---
+++ quene/src/quene/TheQuene.java
@@ -0,0 +1,23 @@
+package quene;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-08-05
+ * Time: 19:14
+ */
+public class TheQuene {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+
+}
