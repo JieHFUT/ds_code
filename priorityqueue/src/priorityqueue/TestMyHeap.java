e16a9a7c0e31f86df5b58e5d2cee491c2d3ad575
jieHFUT <3492779706@qq.com>
2022-08-09 21:07:42 +0800

    堆

---
+++ priorityqueue/src/priorityqueue/TestMyHeap.java
@@ -0,0 +1,27 @@
+package priorityqueue;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-08-09
+ * Time: 20:31
+ */
+public class TestMyHeap {
+
+    public static void main(String[] args) {
+
+        int[] array = { 27,15,19,18,28,34,65,49,25,37 };
+        MyHeap myHeap = new MyHeap();
+
+
+
+
+
+
+
+
+
+
+    }
+}
